8a25dbfb3c3d463a1e6ac267a4f4dec19092f7da
KotniJagadeesh20 <jagadeeshkotni20@gmail.com>
2024-08-26 21:09:57 +0530

    Chnages on js file

--- Js/Script.js
+++ Js/Script.js
@@ -1,34 +1,39 @@
-let menu=document.querySelector('#menu-bars');
-let header=document.querySelector('header');
 
-menu.onclick=()=>{
-    menu.classList.toggle('fa-times');
-    header.classList.toggle('active');
-}
 
-window.onscroll=()=>{
-    menu.classList.remove('fa-times');
-    header.classList.remove('active');    
-}
-
-let cursor1=document.querySelector('.cursor-1');
-let cursor2=document.querySelector('.cursor-2');
-
-window.onmousemove=(e)=>{
-    cursor1.style.top =e.pageY+'px';
-    cursor1.style.left=e.pageX+'px';
-    cursor2.style.top =e.pageY+'px';
-    cursor2.style.left=e.pageX+'px';
-}
-let links=document.querySelectorAll('a').forEach(links=>{
-
-    links.onmouseenter=()=>{
+document.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('#menu-bars');
+    const header = document.querySelector('header');
+  
+    menu.addEventListener('click', () => {
+      menu.classList.toggle('fa-times');
+      header.classList.toggle('active');
+    });
+  
+    window.addEventListener('scroll', () => {
+      menu.classList.remove('fa-times');
+      header.classList.remove('active');
+    });
+  
+    const cursor1 = document.querySelector('.cursor-1');
+    const cursor2 = document.querySelector('.cursor-2');
+  
+    window.addEventListener('mousemove', (e) => {
+      cursor1.style.top = `${e.pageY}px`;
+      cursor1.style.left = `${e.pageX}px`;
+      cursor2.style.top = `${e.pageY}px`;
+      cursor2.style.left = `${e.pageX}px`;
+    });
+  
+    document.querySelectorAll('a').forEach((link) => {
+      link.addEventListener('mouseenter', () => {
         cursor1.classList.add('active');
         cursor2.classList.add('active');
-    }
-
-    links.onmouseleave=()=>{
+      });
+  
+      link.addEventListener('mouseleave', () => {
         cursor1.classList.remove('active');
         cursor2.classList.remove('active');
-    }
-});
+      });
+    });
+  });
+  
